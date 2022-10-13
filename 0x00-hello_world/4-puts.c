537619d1e76af6c0aa5c55aa6cfdcef0b00b7675
Unuigboje <onpet@yahoo.com>
2022-10-13 02:00:40 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #!/bin/bash
 #include <stdio.h>
+/**
+ *main-Printing
+ *
+ * Return: Always 0 (Successful)
+ */
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
-	return(0);
-}    
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);   
+
+}	
