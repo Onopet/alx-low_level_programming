70120006bbb02fe8435e6d4803230b5d7fff48ac
Unuigboje <onpet@yahoo.com>
2022-10-13 02:47:07 -0700

    my first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 0 (Successful)
  */
-int main (void)
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);   
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }	
